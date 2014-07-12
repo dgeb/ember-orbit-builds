abed22370d13f4333d01d01770e640402b0263e5
Dan Gebhardt <dan@cerebris.com>
2014-07-12 01:59:34 -0400

    0.2.1

--- ember-orbit.amd.js
+++ ember-orbit.amd.js
@@ -963,17 +963,15 @@ define("ember-orbit/source",
         this._super.apply(this, arguments);
 
         var OrbitSourceClass = get(this, 'orbitSourceClass');
+
         // If `orbitSourceClass` is obtained through the _super chain, dereference
         // its wrapped function, which will be the constructor.
         //
         // Note: This is only necessary when retrieving a *constructor* from an Ember
         //       class hierarchy. Otherwise, `superWrapper` "just works".
         if (OrbitSourceClass && OrbitSourceClass.wrappedFunction) {
-         OrbitSourceClass = OrbitSourceClass.wrappedFunction;
+          OrbitSourceClass = OrbitSourceClass.wrappedFunction;
         }
-
-        Ember.assert("Source.orbitSourceClass must be initialized with an instance of an `OC.Source`",
-          OrbitSourceClass); // TODO: verify OrbitSourceClass instanceof OCSource - need to resolve an inheritance problem in Orbit first
 
         var schema = get(this, 'schema');
         if (!schema) {
@@ -985,6 +983,9 @@ define("ember-orbit/source",
         var orbitSourceSchema = get(schema, '_schema');
         var orbitSourceOptions = get(this, 'orbitSourceOptions');
         this.orbitSource = new OrbitSourceClass(orbitSourceSchema, orbitSourceOptions);
+
+        Ember.assert("orbitSource must be an instance of an `OC.Source`",
+          this.orbitSource instanceof OCSource);
       }
     });
 
